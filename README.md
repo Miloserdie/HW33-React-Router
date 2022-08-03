33a6ce0e6d52b084223de651fb88c8a64ec8ede3
Miloserdie <89547280+Miloserdie@users.noreply.github.com>
2022-08-04 01:53:02 +0300

    done

---
+++ README.md
@@ -0,0 +1,20 @@
+Создайте на вашем github репозиторий по следующему шаблону HW#-name. Все результаты нужно запушить в ваш репозиторий и прикрепить ссылку на hillel портале.
+Сгенерируйте react приложение. Обратите внимание что нужно установить yarn или npm пакеты после генерации проекта.
+Создайте README.md с описанием задания.
+Выбирите один из вариантов 1 или 2.
+Вариант 1. Реализуйте приложение Health APP. 
+На Home Page нужно чтобы отображалось четыре плашки (можно кнопки) которые будут делать редирект на следующие разделы: Appointments, Events, Doctors, Clients.
+Сделайте разный контент для разделов Appointments, Events, Doctors, Clients
+Используйте по выбору одно из библиотек UI Components. Используйте одну из них для стилизации вашего приложения.
+https://react-bootstrap.github.io/getting-started/introduction/
+https://ant.design/docs/react/getting-started
+https://mui.com/getting-started/installation/
+Вариант 2. Переведите туду лист на роутинг
+При клике по имени Todo нужно сделать редирект на отдельную страницу с описанием выбранной задачи.
+Добавьте кнопку Back на странице Todo Details что бы можно было вернутся к основному списку задач.
+Добавить кнопку изминения статуса задачи возле кнопки удалить.
+Используйте по выбору одно из библиотек UI Components. Используйте одну из них для стилизации вашего приложения.
+https://react-bootstrap.github.io/getting-started/introduction/
+https://ant.design/docs/react/getting-started
+https://mui.com/getting-started/installation/
+y
---
+++ src/components/todo/TaskDiscription.jsx
@@ -0,0 +1,82 @@
+import { useParams, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { getTaskDiscriptionReqAction, updateTaskDiscriptionReqAction } from '../../store/actions';
+import './TaskDiscription.scss'
+import { Button } from '@mui/material';
+import SaveIcon from '@mui/icons-material/Save';
+
+function TaskDiscription() {
+	const dispatch = useDispatch(); 
+	const {id} = useParams();
+	const [todo, setTodo] = useState({});
+	const status = todo.status ? 'done' : 'in-progress'; 
+	const [inputValue, setInputValue] = useState(todo.discription);
+	const [btnBlock, setBtnBlock] = useState(false);
+
+	useEffect(() => {
+		async function getTask() {
+			const todo = await dispatch(getTaskDiscriptionReqAction(id))
+
+			setTodo(todo);
+			setInputValue(todo.discription);
+		} 
+
+		getTask()
+	}, [id, dispatch])  //Тут таккож просив дописати 
+	
+	
+	function changeValue(e) {
+		setInputValue(e.target.value);
+	}
+
+	function saveValue(e) {
+		setBtnBlock(true)
+		e.preventDefault();
+		const newTask = {
+			id: id,
+			task: todo.task,
+			discription: inputValue,
+			status: todo.status
+		}
+
+		async function updateTask(id, task) {
+			dispatch(updateTaskDiscriptionReqAction(id, task))
+		}
+
+		updateTask(id, newTask);
+
+		setTimeout(() => setBtnBlock(false), 1000);
+	}
+
+	return (
+		<section className='task-discription'>
+			<div className='task-discription__info'>
+				<Link className={`task-discription__link-back btn ${status}`} to='/'>BACK</Link>
+				<h2 className='task-discription__title'>Task: {todo.task}</h2>
+				<form className='task-discription__line'>
+					<textarea onChange={changeValue} className={`task-discription__text ${status}`} value={inputValue}></textarea>
+					<Button 
+						sx={{
+							width: 120,
+							display: 'flex',
+							padding: '8px 10px 5px 10px',
+							color: 'white',
+							background: 'none',
+							":hover": {
+								background: 'rgb(0, 182, 121)',
+							}
+				 	 	}} 
+						startIcon={<SaveIcon />}
+						disabled={btnBlock}
+						onClick={saveValue} 
+						className={`task-discription__btn btn ${status}`}>
+						Save
+					</Button>
+				</form>
+			</div>
+		</section>
+	)
+}
+
+export default TaskDiscription;
